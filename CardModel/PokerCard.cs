f9b43e37af87c1d97fe4379d6b01003b525efe2f
Igor Sotarev <igor@sotarev.ru>
2021-05-06 13:30:41 +0500

    add poker deck class

---
+++ CardModel/PokerCard.cs
@@ -0,0 +1,17 @@
+﻿namespace UdvDeck.CardModel
+{
+    public class PokerCard : Card
+    {
+        public PokerCard(string value, PokerSuit suit) : base(value, suit)
+        {
+        }
+    }
+
+    public enum PokerSuit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+}
---
+++ DeckModel/PokerDeck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UdvDeck.CardModel;
+
+namespace UdvDeck.DeckModel
+{
+    public class PokerDeck : Deck
+    {
+        private readonly string[] _values = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+        
+        public PokerDeck(string name) : base(name, 52)
+        {
+            SortDeck();
+        }
+
+        public void SortDeck()
+        {
+            var i = 0;
+            foreach (PokerSuit suit in Enum.GetValues(typeof(PokerSuit)))
+                foreach(var value in _values)
+                    Cards[i++] = new PokerCard(value, suit);
+        }
+    }
+}
