3597649a7a87c98c3e9b1b0c8fbbeab63ad4275a
Igor Sotarev <igor@sotarev.ru>
2021-05-06 11:26:17 +0500

    initial commit

---
+++ CardModel/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UdvDeck.CardModel
+{
+    public abstract class Card
+    {
+        public string Value { get; }
+        public Enum Suit { get; }
+
+        protected Card(string value, Enum suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+        
+        public override string ToString()
+        {
+            return $"{Value} of {Suit}";
+        }
+    }
+}
---
+++ DeckModel/Deck.cs
@@ -0,0 +1,16 @@
+﻿using UdvDeck.CardModel;
+
+namespace UdvDeck.DeckModel
+{
+    public abstract class Deck
+    {
+        public Card[] Cards { get; }
+        public string Name { get; }
+
+        protected Deck(string name, int cardsCount)
+        {
+            Name = name;
+            Cards = new Card[cardsCount];
+        }
+    }
+}
