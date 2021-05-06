54a2068d7798bfa508c9f52a138158e8b2430891
Igor Sotarev <igor@sotarev.ru>
2021-05-06 20:17:00 +0500

    create restful service

---
+++ Controller.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UdvDeck
+{
+    [ApiController]
+    [IgnoreAntiforgeryToken]
+    public class Controller : ControllerBase
+    {
+        private static readonly DeckController DeckController = new DeckController();
+        
+        [HttpPut("")]
+        public IActionResult Create([FromBody] DeckName arg)
+        {
+            var name = arg.Name;
+
+            if (DeckController.ContainsName(name))
+                return BadRequest();
+            
+            DeckController.CreateDeck(name);
+            return Created($"~/{name}", Deck(name));
+        }
+
+        public class DeckName
+        {
+            public string Name { get; set; }
+        }
+
+        [HttpDelete("{name}")]
+        public IActionResult Delete(string name)
+        {
+            if (!DeckController.ContainsName(name))
+                return NotFound();
+                
+            DeckController.DeleteDeck(name);
+            return Ok();
+        }
+
+        [HttpGet("")]
+        public IEnumerable<string> List()
+        {
+            return DeckController.GetDeckList();
+        }
+
+        [HttpPatch("{name}")]
+        public IActionResult Shuffle(string name)
+        {
+            if (!DeckController.ContainsName(name))
+                return NotFound();
+            
+            DeckController.ShuffleDeck(name);
+            return Ok();
+        }
+
+        [HttpGet("{name}")]
+        public ActionResult<string[]> Deck(string name)
+        {
+            if (!DeckController.ContainsName(name))
+                return NotFound();
+            
+            return DeckController.GetDeck(name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace UdvDeck
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
